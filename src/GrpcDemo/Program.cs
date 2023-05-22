a3ba8164d7d0bdc616308d3e145e52637b80a6cb
SIkebe <SIkebe@users.noreply.github.com>
2023-05-22 14:21:06 +0900

    Configure Kestrel to listen on a specific Http2 only port

--- src/GrpcDemo/Program.cs
+++ src/GrpcDemo/Program.cs
@@ -1,6 +1,22 @@
 using GrpcDemo.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Kestrel to listen on a specific Http2 only port
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080);
+    options.ListenAnyIP(8585, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http2;
+    });
+});
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
 builder.Services.AddGrpc();
 
 var app = builder.Build();
